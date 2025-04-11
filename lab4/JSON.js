4791ba3ac1bf87b975267fc7e9646f5cff52e0c6
Cody Bruns <ckb010@tamu.edu>
2025-04-11 13:38:20 -0500

    lab 4 work

---
+++ lab4/JSON.js
@@ -0,0 +1,21 @@
+var JSON =  
+{
+    "data":
+    [
+        {
+            "name": "dan",
+            "age": 39,
+            "gpa": 4.0
+        },
+        {
+            "name": "paul",
+            "age": 55,
+            "gpa": 3.0
+        },
+        {
+            "name": "edgar",
+            "age": 25,
+            "gpa": 2.0
+        }
+    ]
+}
